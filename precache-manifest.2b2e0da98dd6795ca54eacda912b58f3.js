af756e486e1a1bb5aa377f784cc276adf43e6db0
Alex Crocker <croc122@gmail.com>
2019-03-28 14:04:11 -0400

    Updates

---
+++ precache-manifest.2b2e0da98dd6795ca54eacda912b58f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4be15b3581197548b33d",
+    "url": "/cncmachines-takehome-project/static/js/runtime~main.e679e5ad.js"
+  },
+  {
+    "revision": "e17ae9bbaf6b2d992401",
+    "url": "/cncmachines-takehome-project/static/js/main.65c5be78.chunk.js"
+  },
+  {
+    "revision": "3ee95886aa81bd87dfd6",
+    "url": "/cncmachines-takehome-project/static/js/2.33eac1b0.chunk.js"
+  },
+  {
+    "revision": "e17ae9bbaf6b2d992401",
+    "url": "/cncmachines-takehome-project/static/css/main.89bab82b.chunk.css"
+  },
+  {
+    "revision": "5247bc0d6ce2be9d1cfcd49b040af0b9",
+    "url": "/cncmachines-takehome-project/index.html"
+  }
+];
